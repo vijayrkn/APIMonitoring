84222dd43ffbd33ce930b9c38d271fcd5ae6c43a
Vijay Ramakrishnan <vramak@microsoft.com>
2022-05-01 15:24:50 -0700

    Read the API details from Configuration

--- APITrigger/APITimerTrigger.cs
+++ APITrigger/APITimerTrigger.cs
@@ -1,16 +1,31 @@
 using System;
+using System.Net.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace APITrigger
 {
     public class APITimerTrigger
     {
+        private IConfiguration _configuration;
+
+        public APITimerTrigger(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [FunctionName("APITimerTrigger")]
         public void Run([TimerTrigger("*/3 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            string api = _configuration["API"];
+            if (!string.IsNullOrEmpty(api))
+            {
+                HttpClient client = new HttpClient();
+                client.GetAsync(api);
+            }
         }
     }
 }
